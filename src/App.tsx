e2ebaa93934436476b3814aee23fb93129c38f8b
ganinw13120 <gan131201@gmail.com>
2021-10-07 08:41:02 +0700

    Project structure

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+export default class App extends Component {
+  render() : JSX.Element  {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p className="title">
+            COMING SOON
+          </p>
+          <p className="description">
+            DatabaseCamp.io
+          </p>
+        </header>
+      </div>
+    );
+  }
+}
