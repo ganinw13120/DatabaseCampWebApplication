47c5da60cde65811f0937d1cd86da4c47efe9916
ganinw13120 <gan131201@gmail.com>
2021-10-12 21:45:40 +0700

    overview structure

--- src/presentation/view/overview/OverviewPage.tsx
+++ src/presentation/view/overview/OverviewPage.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import BaseView from '../BaseView';
-import AuthViewModel from '../../view-model/auth/AuthViewModel';
 
 export interface OverviewComponentState {
 
@@ -9,17 +8,12 @@ export interface OverviewComponentState {
 export default class OverviewPage extends React.Component<any, OverviewComponentState>
   implements BaseView {
   
-  private authViewModel: AuthViewModel;
-  
   public constructor(props: any) {
     super(props);
-    const authViewModel = new AuthViewModel();
-    this.authViewModel = authViewModel;
     
   }
   
   public componentDidMount(): void {
-    this.authViewModel.attachView(this);
   }
 
   public onViewModelChanged(): void {
