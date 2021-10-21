2e9956fbdedb1cae5ec7edda967d0d5ea749876d
ganinw13120 <gan131201@gmail.com>
2021-10-21 15:32:13 +0700

    change store and login responsive

--- src/domain/interactors/auth/LoginUseCase.tsx
+++ src/domain/interactors/auth/LoginUseCase.tsx
@@ -11,23 +11,4 @@ export default class LoginUseCase {
     this.authStore = authStore;
   }
 
-  public async Login(email: string, password: string): Promise<{issuccess : boolean, message? : string}> {
-    const result = await this.authRepository.Login({
-      email: email,
-      password : password,
-    }).then((res) => {
-      this.authStore.Login(res)
-      return {
-        issuccess: true,
-        message : ''
-      }
-    }).catch((err) => {
-      return {
-        issuccess: false,
-        message : err.message
-      }
-    })
-    // console.log(this.authStore)
-    return result
-  }
 }
