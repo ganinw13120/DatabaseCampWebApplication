761f614915ed9452b6e1e17e728aa3de082d7ad8
ganinw13120 <gan131201@gmail.com>
2021-11-19 15:50:08 +0700

    remove useless constructor

--- src/view/error/notFound.tsx
+++ src/view/error/notFound.tsx
@@ -1,27 +1,10 @@
 import React from 'react';
-import BaseView from '@view/BaseView';
 import Navbar from './components/Navbar';
 import Title from './components/Title';
 
 import './notfound.css';
 
-export interface LandingComponentProps {
-}
-
-export interface LandingComponentState {
-
-}
-
-export default class NotFoundPage extends React.Component<LandingComponentProps, LandingComponentState>
-  implements BaseView {
-  public constructor(props: LandingComponentState) {
-    super(props);
-  }
-  public onViewModelChanged(): void {
-    
-  }
-  componentDidMount() {
-  }
+export default class NotFoundPage extends React.Component<{}, {}> {
   public render(): JSX.Element {
     return (
       <>
