30838fd5eb056f997514302a9ef3231d0e9a16be
ganinw13120 <gan131201@gmail.com>
2021-12-05 01:13:57 +0700

    add comments on index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,8 @@
+// index.tsx
+/**
+ * This file is an entry point of application, appending to document, forwarding entry point to app.tsx
+*/
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
