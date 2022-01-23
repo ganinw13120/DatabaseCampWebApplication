d2e03e587d7aba32097f20c67dfe88c2b89d5e10
ganinw13120 <gan131201@gmail.com>
2022-01-23 15:01:36 +0700

    update api endpoints

--- src/constant/constant.ts
+++ src/constant/constant.ts
@@ -2,4 +2,5 @@
 /**
  * This file used be store constant using in application logics.
 */
+// export const API_BASE_URL = "http://localhost:8080/api/v1"
 export const API_BASE_URL = "http://128.199.217.116:8080/api/v1"
